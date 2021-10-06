1ac4bda736b6ab8098ba4409b616ec137e175322
raymo211 <raymond.molina@mycit.ie>
2021-10-06 12:48:18 +0100

    lab 2

--- domain/darts_match.py
+++ domain/darts_match.py
@@ -1,6 +1,19 @@
 class DartsMatch:
 
-    def __init__(self, type, player1, player2):
-        self.type = type
-        self.player1 = player1
-        self.player2 = player2
+    def __init__(self):
+        self.active = True
+        self.players = []
+        self.last_player_index = -1
+        self.visits = []
+        self.winning_num_darts = -1
+        self.winning_player_index = -1
+
+    def register_palyer(self, username):
+        if username not in self.players:
+            index = len(self.players)
+            self.players.append(username)
+            self.visits.append([])
+            return index
+        else:
+            return -1
+        
